8e78fcb599b3d80d5032303226c0ad7b1b6a2c8f
Glatigny <f.west@live.fr>
2019-02-05 15:15:54 +0100

    correction exo 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 $(function(){
-        $('#clickBtnMore, #clickBtnLess').click(function(){
-            var $number = $('#clickNmb').text();
-            if($(this).attr('id') == 'clickBtnMore'){ // si on clique sur le bouton +
-                $number++; // augmenter la valeur
-                $('#clickNmb').text($number);
-            }else{ // ou sinon
-                $number--; // baisser la valeur
-                $('#clickNmb').text($number);
-            }
-        });
+  var count = 0;
+  $('#buttonUp').click(function(){ //clique sur le bouton +
+    count++; //ajoute 1 au compteur
+    $('#number').val(count);
+  });
+  $('#buttonDown').click(function(){ //clique sur le bouton -
+    count--; // retire 1 au compteur
+    $('#number').val(count);
+  });
 });
